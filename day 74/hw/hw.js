4eff8e6898244985358b08f8eff1b3946ad08aea
dato201goal10 <datucha2013jamera@gmail.com>
2025-10-02 22:08:32 +0400

    commit

---
+++ day 74/hw/hw.js
@@ -0,0 +1,113 @@
+// let num1 = 10;
+// let num2 = 5;
+// let operator = "/";
+// let result = "";
+
+// switch (operator) {
+//     case "+":
+//         result = num1 + num2;
+//         break;
+//     case "-":
+//         result = num1 - num2;
+//         break;
+//     case "*":
+//         result = num1 * num2;
+//         break;
+//     case "/":
+//         result = num1 / num2;
+//         break;
+//     case "%":
+//         result = num1 % num2;
+//         break;
+//     default:
+//         result = "Invalid operator";
+// }
+
+// console.log("Result:", result);
+
+// let E = "e";
+
+// switch (E.toLowerCase()) {
+//     case "a":
+//     case "e":
+//     case "i":
+//     case "o":
+//     case "u":
+//         console.log(E, "is a vowel");
+//         break;
+//     default:
+//         console.log(E, "is a consonant");
+// }
+
+// let month = 9;
+// let season = "";
+
+// switch (month) {
+//     case 12:
+//     case 1:
+//     case 2:
+//         season = "Winter";
+//         break;
+//     case 3:
+//     case 4:
+//     case 5:
+//         season = "Spring";
+//         break;
+//     case 6:
+//     case 7:
+//     case 8:
+//         season = "Summer";
+//         break;
+//     case 9:
+//     case 10:
+//     case 11:
+//         season = "Autumn";
+//         break;
+//     default:
+//         season = "Invalid month number";
+// }
+
+// console.log("Season:", season);
+
+
+// let fruit = "apple";
+// let price = "";
+
+// switch (fruit.toLowerCase()) {
+//     case "apple":
+//         price = "2$";
+//         break;
+//     case "banana":
+//         price = "1$";
+//         break;
+//     case "mango":
+//         price = "3$";
+//         break;
+//     default:
+//         price = "Fruit not available";
+// }
+
+// console.log(fruit, "price is", price);
+
+
+// let num = 7;
+// let result = (num % 2 === 0) ? "Even" : "Odd";
+// console.log(num, "is", result);
+
+
+// let number = 67;
+// let check = (number > 0) ? "Positive" 
+//         : (number < 0) ? "Negative" 
+//         : "Zero";
+// console.log(number, "is", check);
+
+
+// let a = 10;
+// let b = 20;
+// let greater = (a > b) ? a : b;
+// console.log("Greater number is:", greater);
+
+
+// let age = 16;
+// let canVote = (age >= 18) ? "Eligible to vote" : "Not eligible to vote";
+// console.log("At age", age, "you are:", canVote);
