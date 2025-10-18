92cdcd61c81ecc3fcae435f05b26af8b042e6416
dato201goal10 <datucha2013jamera@gmail.com>
2025-10-18 18:14:28 +0400

    commit

---
+++ day 76/cw/cw.js
@@ -0,0 +1,14 @@
+// function calculateOperations(operations) {
+//     let results = [];
+//     for (let op of operations) {
+//         results.push(eval(op));
+//     }
+//     return results;
+// }
+
+// const ops = ["2 + 3", "10 - 4", "5 * 6", "20 / 4"];
+// console.log(calculateOperations(ops));
+
+let userInput = prompt("Enter a number:");
+let number = parseInt(userInput);
+console.log("The number is:", number);
---
+++ day 76/hw/hw.js
@@ -0,0 +1,55 @@
+    // let userInput = prompt("Enter a math expression (e.g. 3 + 5 * 2):");
+    // let result = eval(userInput);
+    // console.log("Result:", result);
+
+    // let expression = prompt("Enter a full expression (e.g. 10 + 3 * 2):");
+    // let calcResult = eval(expression);
+    // console.log("Calculated Result:", calcResult);
+
+    // let str = "10 + 5";
+    // console.log(str, "=", eval(str));
+
+    // let comparison = eval("10 > 3");
+    // console.log("Is 10 > 3?", comparison);
+
+    // try {
+    // let invalid = eval("2 ++ 3");
+    // console.log("Invalid expression result:", invalid);
+    // } catch (error) {
+    // console.log("Error caught:", error.message);
+    // }
+
+    // let userInput = prompt("Enter a number (as text):");
+    // let num = parseFloat(userInput);
+    // console.log("Converted number:", num);
+
+    // let pi = parseFloat("3.14");
+    // console.log(pi);
+
+    // let mixed = parseFloat("42abc");
+    // console.log(mixed);
+
+    // let lettersFirst = parseFloat("abc42");
+    // console.log(lettersFirst);
+
+    // let n1 = parseFloat("10.5");
+    // let n2 = parseFloat("20.2");
+    // let n3 = parseFloat("30.8");
+    // let sum = n1 + n2 + n3;
+    // console.log("Sum:", sum);
+
+
+    // let num1 = parseInt("25.9");
+    // console.log(num1);
+
+    // let num2 = parseInt("100abc");
+    // console.log(num2);
+
+    // let num3 = parseInt("abc100");
+    // console.log(num3);
+
+    // let a = parseInt("10");
+    // let b = parseInt("20");
+    // let c = parseInt("30");
+    // let total = a + b + c;
+    // console.log("Total:", total);
